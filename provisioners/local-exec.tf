36dc7a055c0dac49064b71722484c95df9b47907
ashwani1218 <ashwanipandey1218@gmail.com>
2022-01-15 15:52:01 +0530

    added provisioner types

---
+++ provisioners/local-exec.tf
@@ -0,0 +1,11 @@
+
+resource "aws_instance" "myec2" {
+  ami           = "<ami_id>"
+  instance_type = "t2.micro"
+
+  provisioner "local-exec" {
+    command = "echo ${aws_instance.myec2.private_ip} >> private_ips.txt"
+  }
+}
+
+
