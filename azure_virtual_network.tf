3d7e3b13ee065e15d825733cd94f26f9b5cacb4a
ashwani1218 <ashwanipandey1218@gmail.com>
2022-02-01 13:18:21 +0530

    Update azure_virtual_network.tf

--- azure_virtual_network.tf
+++ azure_virtual_network.tf
@@ -1,3 +1,15 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "2.94.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  # Configuration options
+}
 resource "azurerm_virtual_network" "myterraformnetwork" {
   name                = "myvnet"
   address_space       = ["10.0.0.0/16"]
