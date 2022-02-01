7fb087c3735f6aea4b3ad3b40475eb5ce702138c
ashwani1218 <ashwanipandey1218@gmail.com>
2022-02-01 22:17:35 +0530

    updated Virtual network

--- azure/virtual_network/azure_virtual_network.tf
+++ azure/virtual_network/azure_virtual_network.tf
@@ -8,11 +8,11 @@ terraform {
 }
 
 provider "azurerm" {
-  # Configuration options
+  features {}
 }
 resource "azurerm_virtual_network" "myterraformnetwork" {
   name                = "myvnet"
   address_space       = ["10.0.0.0/16"]
   location            = "East US"
-  resource_group_name = "er-tyjy"
+  resource_group_name = "my-resource-group"
 }
