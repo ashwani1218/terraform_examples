03ecc7264fd7b887e57beb133c49ad1bba8035c1
ashwani1218 <ashwanipandey1218@gmail.com>
2022-02-02 12:45:39 +0530

    Azure compute

---
+++ azure/compute/README.md
@@ -0,0 +1,4 @@
+# Compute
+
+This module will create resource group along with a network interface and a linux server.
+It will also output the public dns name of the linux server created.
---
+++ azure/compute/main.tf
@@ -0,0 +1,25 @@
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "example-resources"
+  location = "East Us"
+}
+
+module "linuxservers" {
+  source              = "Azure/compute/azurerm"
+  resource_group_name = azurerm_resource_group.rg.name
+  vm_os_simple        = "UbuntuServer"
+  public_ip_dns       = ["linsimplevmips"] // change to a unique name per datacenter region
+  vnet_subnet_id      = module.network.vnet_subnets[0]
+  depends_on          = [azurerm_resource_group.rg]
+}
+module "network" {
+  source              = "Azure/network/azurerm"
+  resource_group_name = azurerm_resource_group.rg.name
+  subnet_prefixes     = ["10.0.1.0/24"]
+  subnet_names        = ["subnet1"]
+
+  depends_on = [azurerm_resource_group.rg]
+}
