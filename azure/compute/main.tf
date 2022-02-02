ee953a8a9b5d1488a2aabe9716c9f3c3d47ef8bf
ashwani1218 <ashwanipandey1218@gmail.com>
2022-02-02 13:39:01 +0530

    updated compute for azure

--- azure/compute/main.tf
+++ azure/compute/main.tf
@@ -7,19 +7,63 @@ resource "azurerm_resource_group" "rg" {
   location = "East Us"
 }
 
-module "linuxservers" {
-  source              = "Azure/compute/azurerm"
+resource "azurerm_virtual_network" "vnet" {
+  name                = "${var.prefix}-network"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
-  vm_os_simple        = "UbuntuServer"
-  public_ip_dns       = ["linsimplevmips"] // change to a unique name per datacenter region
-  vnet_subnet_id      = module.network.vnet_subnets[0]
-  depends_on          = [azurerm_resource_group.rg]
 }
-module "network" {
-  source              = "Azure/network/azurerm"
-  resource_group_name = azurerm_resource_group.rg.name
-  subnet_prefixes     = ["10.0.1.0/24"]
-  subnet_names        = ["subnet1"]
 
-  depends_on = [azurerm_resource_group.rg]
+resource "azurerm_subnet" "internal" {
+  name                 = "internal"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.2.0/24"]
 }
+
+resource "azurerm_network_interface" "ninterface" {
+  name                = "${var.prefix}-nic"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "testconfiguration1"
+    subnet_id                     = azurerm_subnet.internal.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+resource "azurerm_virtual_machine" "main" {
+  name                  = "${var.prefix}-vm"
+  location              = azurerm_resource_group.rg.location
+  resource_group_name   = azurerm_resource_group.rg.name
+  network_interface_ids = [azurerm_network_interface.ninterface.id]
+  vm_size               = "Standard_DS1_v2"
+
+  delete_os_disk_on_termination    = true
+  delete_data_disks_on_termination = true
+
+  storage_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "16.04-LTS"
+    version   = "latest"
+  }
+  storage_os_disk {
+    name              = "myosdisk1"
+    caching           = "ReadWrite"
+    create_option     = "FromImage"
+    managed_disk_type = "Standard_LRS"
+  }
+  os_profile {
+    computer_name  = "hostname"
+    admin_username = "testadmin"
+    admin_password = "Password1234!"
+  }
+  os_profile_linux_config {
+    disable_password_authentication = false
+  }
+  tags = {
+    environment = "staging"
+  }
+}
+
--- azure/compute/outputs.tf
+++ azure/compute/outputs.tf
@@ -1,4 +1,15 @@
+output "resource_group" {
+  value = azurerm_resource_group.rg.name
+}
+output "subnet" {
+  value = azurerm_subnet.internal.name
+}
+
+output "network_interface" {
+  value = azurerm_network_interface.ninterface.name
+}
+
 
 output "linux_vm_public_name" {
-  value = module.linuxservers.public_ip_dns_name
+  value = azurerm_virtual_machine.main.name
 }
